81018aaa96f9765d0ed64c961c2ff366c0ac46a3
Fredrik Nilson <sassur@gmail.com>
2011-10-26 09:48:45 +0200

    adding Electoral Register adapter

--- sverok_rm/__init__.py
+++ sverok_rm/__init__.py
@@ -5,3 +5,4 @@ def includeme(config):
     cache_ttl_seconds = int(config.registry.settings.get('cache_ttl_seconds', 7200))
     config.add_static_view('rm_static', 'voteit_rm:static', cache_max_age = cache_ttl_seconds)
     config.scan(__name__)
+    config.include('sverok_rm.models.electoral_register')
