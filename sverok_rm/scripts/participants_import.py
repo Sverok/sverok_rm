24074c75c1983964b4cef6d6c38c25d34a1e06b9
Fredrik Nilson <sassur@gmail.com>
2011-11-01 13:21:17 +0100

    fixes after new participant file

--- sverok_rm/scripts/participants_import.py
+++ sverok_rm/scripts/participants_import.py
@@ -22,34 +22,29 @@ def participants_import(*args):
     users = worker.root.users
     meeting = worker.root[meetingname]
 
-    reader = csv.reader(open(filename, "rb"), delimiter=',', quotechar='"')
+    reader = csv.reader(open(filename, "rb"), delimiter=';', quotechar='"')
     
     try:
         # skip first line
         reader.next()
         for participant in reader:
-            firstname = unicode(participant[0], 'utf-8')
-            lastname = unicode(participant[1], 'utf-8')
-            email = participant[2]
-            userid = participant[3]
-            if len(participant) > 4:
-                reserve = participant[4]
-            else:
-                reserve = 100
-            #FIXME: get password from file
+            userid = participant[0]
+            firstname = unicode(participant[2], 'utf-8')
+            lastname = unicode(participant[3], 'utf-8')
+            email = participant[4]
+            password = unicode(participant[5], 'utf-8')
             
             print "\timporting: %s - %s %s" % (userid, firstname, lastname)
-
+            
             user = User()
             user.set_field_value('first_name', firstname)
             user.set_field_value('last_name', lastname)
             user.set_field_value('email', email)
-            #FIXME: get password from file
-            user.set_password('dummy')
+            user.set_password(password)
             
             users[userid] = user
             
-            meeting.add_groups(userid, (security.ROLE_DISCUSS, security.ROLE_PROPOSE, ))
+            meeting.add_groups(userid, (security.ROLE_PROPOSE, ))
             
         transaction.commit()
     except Exception, e:
