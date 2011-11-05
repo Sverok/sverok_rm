3bd6158a257a3ccdd5afd8b0f5248112155ce4d4
Robin Harms Oredsson <robin@betahaus.net>
2011-11-05 22:10:56 +0100

    Translations must be loaded

--- sverok_rm/__init__.py
+++ sverok_rm/__init__.py
@@ -5,8 +5,8 @@ SverokMF = TranslationStringFactory('sverok_rm')
 
 
 def includeme(config):
-    #FIXME: Do things with voteit :)
     cache_ttl_seconds = int(config.registry.settings.get('cache_ttl_seconds', 7200))
     config.add_static_view('rm_static', 'voteit_rm:static', cache_max_age = cache_ttl_seconds)
     config.scan('sverok_rm')
     config.include('sverok_rm.models.electoral_register')
+    config.add_translation_dirs('sverok_rm:locale/')
