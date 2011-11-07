0ee5c834d303ea4ba7577b9825b85ec109250012
Fredrik Nilson <sassur@gmail.com>
2011-11-07 14:24:52 +0100

    removing argument check

--- sverok_rm/scripts/participants_import.py
+++ sverok_rm/scripts/participants_import.py
@@ -7,10 +7,6 @@ from voteit.core import security
 
 
 def participants_import(*args):
-    if len(args) != 2:
-        print "2 arguments needed, first the slug of the meeting, second the name of the file"
-        return
-    
     args = list(args)
     meetingname = args[0][0]
     filename = args[0][1]
