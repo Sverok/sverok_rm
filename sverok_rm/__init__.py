172dfd56a22ffdf1499accf1f71b8a21b5b4d79e
robinharms <robin@betahaus.net>
2014-11-05 17:55:39 +0100

    monkey-patching localisation...

--- sverok_rm/__init__.py
+++ sverok_rm/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from pyramid.i18n import TranslationStringFactory
 
 
@@ -10,3 +11,20 @@ def includeme(config):
     config.scan('sverok_rm')
     config.include('sverok_rm.models.elegible_voters_method')
     config.add_translation_dirs('sverok_rm:locale/')
+    config.include(patch_localisation)
+
+def patch_localisation(config):
+    from betahaus.pyracont.interfaces import ISchemaFactory
+    to_patch = ('AddUserSchema', 'RegisterUserSchema', 'EditUserSchema')
+    for name in to_patch:
+        factory = config.registry.getUtility(ISchemaFactory, name = name)
+        cls = factory._callable
+        node = getattr(cls, 'last_name')
+        node.description = u"Här kan du också lägga till ditt pronomen (hon/han/hen) om du vill."
+        node = getattr(cls, 'about_me', None)
+        if node:
+            node.description = """Här kan du fylla i mer information om dig som kan vara intressant för andra att veta.
+                Det kan vara vilken förening du tillhör, vilket pronomen du föredrar (han/hon/hen)
+                eller om du har någon särskild hjärtefråga i Sverok.
+                Vänligen notera att det du skriver här blir synligt för alla användare som
+                är deltagare i samma möte som du."""
